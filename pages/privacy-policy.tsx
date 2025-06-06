0e8563495913e9602efaf5c70aabf64680748ba1
theognis1002 <theognis1002@gmail.com>
2025-06-06 11:10:08 -0400

    Adjust privacy policy

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -314,87 +314,54 @@ const Privacypolicy = () => {
                     <div className="pb-12">
                         <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">Privacy Policy</h4>
                         <p className="pb-5 text-lg font-medium">
-                            At www.arclite-technologies.com, accessible at www.arclite-technologies.com, one of our main priorities is the privacy of our
-                            visitors. This Privacy Policy document contains types of information that is collected and recorded by www.arclite-technologies.com
-                            and how we use it.
+                            Our Chrome extension is designed with your privacy in mind. We want to be clear about our strict no-data-collection policy: this extension does not collect, store, or transmit any of your personal information or browsing data.
                         </p>
                         <p className="pb-5 text-lg font-medium">
-                            If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us through email
-                            at admin@ticketbrokerlist.com
+                            If you have any questions about this Privacy Policy, you can contact us at team@wraithscan.com
                         </p>
                         <p className="text-lg font-medium">
-                            This privacy policy applies only to our online activities and is valid for visitors to our website with regards to the information
-                            that they shared and/or collect in www.arclite-technologies.com. This policy is not applicable to any information collected offline
-                            or via channels other than this website.
+                            This privacy policy applies specifically to our Chrome extension and outlines our commitment to protecting your privacy.
                         </p>
                     </div>
                     <div className="pb-12">
-                        <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">Consent</h4>
-                        <p className="pb-5 text-lg font-medium">By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
-                        <p className="text-lg font-medium">
-                            Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to
-                            enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising
-                            partners may also use cookies.
-                        </p>
-                    </div>
-                    <div className="pb-12">
-                        <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">Information we collect</h4>
+                        <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">No Data Collection</h4>
                         <p className="pb-5 text-lg font-medium">
-                            The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you
-                            at the point we ask you to provide your personal information.
+                            Our extension operates entirely on your local machine and does not:
                         </p>
-                        <p className="pb-5 text-lg font-medium">
-                            If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the
-                            contents of the message and/or attachments you may send us, and any other information you may choose to provide.
-                        </p>
-                        <p className="pb-5 text-lg font-medium">
-                            When you register for an Account, we may ask for your contact information, including items such as name, company name, address,
-                            email address, and telephone number.
-                        </p>
-                    </div>
-                    <div className="pb-12">
-                        <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">How we use your information</h4>
-                        <p className="pb-5 text-lg font-medium">We use the information we collect in various ways, including to:</p>
-                        <ul className="text-lg font-medium">
+                        <ul className="text-lg font-medium pb-5">
                             <li className="relative before:absolute before:top-3 before:h-1.5 before:w-1.5 before:rounded-full before:bg-gray ltr:pl-4 ltr:before:left-0 rtl:pr-4 rtl:before:right-0">
-                                Provide, operate, and maintain our website
+                                Collect any personal information
                             </li>
                             <li className="relative before:absolute before:top-3 before:h-1.5 before:w-1.5 before:rounded-full before:bg-gray ltr:pl-4 ltr:before:left-0 rtl:pr-4 rtl:before:right-0">
-                                Improve, personalize, and expand our website
+                                Track your browsing history
                             </li>
                             <li className="relative before:absolute before:top-3 before:h-1.5 before:w-1.5 before:rounded-full before:bg-gray ltr:pl-4 ltr:before:left-0 rtl:pr-4 rtl:before:right-0">
-                                Understand and analyze how you use our website
+                                Store any user data
                             </li>
                             <li className="relative before:absolute before:top-3 before:h-1.5 before:w-1.5 before:rounded-full before:bg-gray ltr:pl-4 ltr:before:left-0 rtl:pr-4 rtl:before:right-0">
-                                Develop new products, services, features, and functionality
-                            </li>
-                            <li className="relative before:absolute before:top-3 before:h-1.5 before:w-1.5 before:rounded-full before:bg-gray ltr:pl-4 ltr:before:left-0 rtl:pr-4 rtl:before:right-0">
-                                Communicate with you, either directly or through one of our partners, including for customer service, to provide you with
-                                updates
+                                Transmit any information to external servers
                             </li>
                         </ul>
-                    </div>
-                    <div className="pb-12">
-                        <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">Links to Other Sites/Apps</h4>
                         <p className="text-lg font-medium">
-                            Our Website links to other websites/Websites that may collect personally identifiable information about you. Mntra is not
-                            responsible for the privacy practices or the content of those linked websites/Websites.
+                            The extension functions entirely within your browser, and we have specifically designed it to respect your privacy.
                         </p>
                     </div>
                     <div className="pb-12">
-                        <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">Security Precautions</h4>
+                        <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">Required Permissions</h4>
+                        <p className="pb-5 text-lg font-medium">
+                            Any permissions requested by the extension are solely for its core functionality. These permissions are used only to perform the extension&apos;s intended features and not for collecting or transmitting data.
+                        </p>
+                    </div>
+                    <div className="pb-12">
+                        <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">Updates</h4>
                         <p className="text-lg font-medium">
-                            Our Website has stringent security measures in place to protect the loss, misuse, and alteration of the information under our
-                            control. Whenever you change or access your account information, we offer the use of a secure server. Once your information is in
-                            our possession we adhere to strict security guidelines, protecting it against unauthorized access.
+                            We may update this Privacy Policy from time to time. Any changes will be reflected in the Chrome Web Store listing, and significant changes will be communicated through the extension&apos;s update notes.
                         </p>
                     </div>
                     <div>
-                        <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">Choice/Opt-Out</h4>
+                        <h4 className="pb-5 text-xl font-bold text-black dark:text-white sm:text-2xl">Contact</h4>
                         <p className="text-lg font-medium">
-                            We provide all users with the opportunity to opt-out of receiving non-essential (promotional, marketing-related) communications from
-                            us on behalf of our partners, and from us in general, after setting up an account. If you want to remove your contact information
-                            from all our lists and newsletters, please visit unsubscribe.
+                            If you have any questions about this Privacy Policy or our practices, please contact us at team@wraithscan.com.
                         </p>
                     </div>
                 </div>
